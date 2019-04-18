1b438b0fc3cd2b353b0daf94d5703011674569a0
Bengt Marten Agren <marangisto@me.com>
2019-04-18 17:31:52 +0800

    Cleaned up blink example.

--- example/blink/Main.cpp
+++ example/blink/Main.cpp
@@ -6,8 +6,8 @@ using namespace rcc;
 
 static void delay_ms(uint32_t x)
 {
-    for (uint32_t j = 0; j < x * 300; ++j)
-        GPIOC.BRR = 0x0000;        // forces evaluation but does nothing!
+    for (volatile uint32_t j = 0; j < x * 3500; ++j)
+        ;
 }
 
 int main()
@@ -15,16 +15,10 @@ int main()
     RCC.AHBENR |= BV(AHBENR::IOPCEN);       // IOPCEN - enable clock on GPIOC
     GPIOC.MODER |= BV(MODER::MODER8) | BV(MODER::MODER9); // bit-0 output mode
 
-    bool on = true;
-
-    for (;;)
+    for (bool on = true; true; on = !on)
     {
+        (on ? GPIOC.BSRR : GPIOC.BRR) = 0x0300;
         delay_ms(500);
-        if (on)
-            GPIOC.BSRR = 0x0300;
-        else
-            GPIOC.BRR = 0x0300;
-        on = !on;
     }
 }
 
