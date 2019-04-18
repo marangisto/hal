7fb3ef14d97a30e0a8ba85636d4bc3ab84e7f2cf
Bengt Marten Agren <marangisto@me.com>
2019-04-18 16:26:36 +0800

    Added HSI-PLL clock setup.

--- src/system.cpp
+++ src/system.cpp
@@ -1,16 +1,28 @@
 #include <stm32f0x1.h>
 
 using namespace stm32f0x1;
-using namespace rcc;
 
 extern "C" void system_init(void)
 {
+    using namespace rcc;
+
     // reset clock control registers
+
     RCC.CR = CR::RESET_VALUE;
     RCC.CFGR = CFGR::RESET_VALUE;
     RCC.CFGR2 = CFGR2::RESET_VALUE;
     RCC.CFGR3 = CFGR3::RESET_VALUE;
     RCC.CR2 = CR2::RESET_VALUE;
     RCC.CIR = CIR::RESET_VALUE;
+
+    // set system clock to HSI-PLL 48MHz
+
+    flash::Flash.ACR = BV(flash::ACR::PRFTBE) | BV(flash::ACR::LATENCY);
+
+    RCC.CFGR |= (0xa << CFGR::PLLMUL);              // PLL multiplier 12
+    RCC.CR |= BV(CR::PLLON);                        // enable PLL
+    while (!(RCC.CR & CR::PLLRDY));                 // wait for PLL to be ready
+    RCC.CFGR |= (0x2 << CFGR::SW);                  // select PLL as system clock
+    while (((RCC.CFGR >> CFGR::SWS) & 0x3) != 0x2); // wait for PLL as system clock
 }
 
