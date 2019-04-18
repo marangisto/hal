85641793b066d87a9d8e0cefe2ac44a359a1dc20
Bengt Marten Agren <marangisto@me.com>
2019-04-18 22:15:00 +0800

    Renamed blink example and use sys_tick delay.

--- example/Blink/Main.cpp
+++ example/Blink/Main.cpp
@@ -4,12 +4,6 @@ using namespace stm32f0x1;
 using namespace gpioc;
 using namespace rcc;
 
-static void delay_ms(uint32_t x)
-{
-    for (volatile uint32_t j = 0; j < x * 3500; ++j)
-        ;
-}
-
 int main()
 {
     RCC.AHBENR |= BV(AHBENR::IOPCEN);       // IOPCEN - enable clock on GPIOC
@@ -18,7 +12,7 @@ int main()
     for (bool on = true; true; on = !on)
     {
         (on ? GPIOC.BSRR : GPIOC.BRR) = 0x0300;
-        delay_ms(500);
+        sys_tick::delay_ms(500);
     }
 }
 
