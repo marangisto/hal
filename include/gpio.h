07cc5d36478a8143061d2ee79d79e20e0aaa670b
Marten Agren <marangisto@me.com>
2019-04-24 04:09:16 +0800

    Partial implementation of alternate functions.

--- include/gpio.h
+++ include/gpio.h
@@ -113,6 +113,141 @@ private:
     typedef pin_t<PORT, BIT> pin;
 };
 
+namespace internal
+{
+
+enum alt_fun_t { AF0, AF1, AF2, AF3, AF4, AF5, AF6, AF7 };
+
+enum alternate_function_t
+    { CAN_RX
+    , CAN_TX
+    , CEC
+    , COMP1_OUT
+    , COMP2_OUT
+    , CRS_SYNC
+    , EVENTOUT
+    , I2C1_SCL
+    , I2C1_SDA
+    , I2C1_SMBA
+    , I2C2_SCL
+    , I2C2_SDA
+    , I2S1_CK
+    , I2S1_MCK
+    , I2S1_SD
+    , I2S1_WS
+    , IR_OUT
+    , MCO
+    , SPI1_MISO
+    , SPI1_MOSI
+    , SPI1_NSS
+    , SPI1_SCK
+    , SPI2_MISO
+    , SPI2_MOSI
+    , SPI2_NSS
+    , SPI2_SCK
+    , SWCLK
+    , SWDIO
+    , TIM14_CH1
+    , TIM15_BKIN
+    , TIM15_CH1
+    , TIM15_CH1N
+    , TIM15_CH2
+    , TIM16_BKIN
+    , TIM16_CH1
+    , TIM16_CH1N
+    , TIM17_BKIN
+    , TIM17_CH1
+    , TIM17_CH1N
+    , TIM1_BKIN
+    , TIM1_CH1
+    , TIM1_CH1N
+    , TIM1_CH2
+    , TIM1_CH2N
+    , TIM1_CH3
+    , TIM1_CH3N
+    , TIM1_CH4
+    , TIM1_ETR
+    , TIM2_CH1_ETR
+    , TIM2_CH2
+    , TIM2_CH3
+    , TIM2_CH4
+    , TIM3_CH1
+    , TIM3_CH2
+    , TIM3_CH3
+    , TIM3_CH4
+    , TIM3_ETR
+    , TSC_G1_IO1
+    , TSC_G1_IO2
+    , TSC_G1_IO3
+    , TSC_G1_IO4
+    , TSC_G2_IO1
+    , TSC_G2_IO2
+    , TSC_G2_IO3
+    , TSC_G2_IO4
+    , TSC_G3_IO2
+    , TSC_G3_IO3
+    , TSC_G3_IO4
+    , TSC_G4_IO1
+    , TSC_G4_IO2
+    , TSC_G4_IO3
+    , TSC_G4_IO4
+    , TSC_G5_IO1
+    , TSC_G5_IO2
+    , TSC_G5_IO3
+    , TSC_G5_IO4
+    , TSC_G6_IO1
+    , TSC_G6_IO2
+    , TSC_G6_IO3
+    , TSC_G6_IO4
+    , TSC_SYNC
+    , USART1_CK
+    , USART1_CTS
+    , USART1_RTS
+    , USART1_RX
+    , USART1_TX
+    , USART2_CK
+    , USART2_CTS
+    , USART2_RTS
+    , USART2_RX
+    , USART2_TX
+    , USART3_CTS
+    , USART4_RTS
+    , USART4_RX
+    , USART4_TX
+    , USART6_RX
+    , USART6_TX
+    };
+
+template<port_enum_t PORT, int BIT, alternate_function_t ALT>
+struct alt_fun_traits {};
+
+#define ALT_FUN_TRAIT(PORT, PIN, ALT_FUN, AFNO)         \
+template<> struct alt_fun_traits<PORT, PIN, ALT_FUN>    \
+{                                                       \
+    static inline alt_fun_t AF() { return AFNO; }       \
+}
+
+#if defined(STM32F051)
+#include "gpio/stm32f051.h"
+#endif
+
+template<port_enum_t PORT, int BIT, alternate_function_t ALT>
+class alternate_t
+{
+public:
+    static inline void setup()
+    {
+        port_traits<PORT>::setup();
+        pin::gpio().MODER |= pin::alternate_mode << (BIT*2);
+        static_assert(PORT != E, "FIXME: use traits to set alternative");
+    }
+
+private:
+    typedef pin_t<PORT, BIT> pin;
+};
+
+}
+
 }
 
 }
