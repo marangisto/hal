7061732d96e32dd0d0d55ae329202ac537c90e3b
Marten Agren <marangisto@me.com>
2019-04-24 04:03:46 +0800

    Utility to produce alternate function trait declarations.

---
+++ util/AltFun.hs
@@ -0,0 +1,34 @@
+module AltFun (main) where
+
+import System.Environment
+import Data.Maybe
+
+main :: IO ()
+main = do
+    [file] <- getArgs
+    xs <- lines <$> readFile file
+    let ys = concatMap (process . words) xs
+    mapM_ putStrLn ys
+
+process (x:xs) = mapMaybe (uncurry $ decl x) $ concatMap multi $ zip xs [0..]
+
+decl p "-" _ = Nothing
+decl p f i = Just $ mconcat
+    [ "ALT_FUN_TRAIT("
+    , port
+    , ", "
+    , pin
+    , ", "
+    , f
+    , ", AF"
+    , show i
+    , ");"
+    ]
+    where (port, pin) = split p
+
+split (_:port:pin) = ([port], pin)
+split _ = ("????", "xxx")
+
+multi :: (String, Int) -> [(String, Int)]
+multi (s, i) = [ (x, i) | x <- words $ map (\c -> if c == ',' then ' ' else c) s ]
+
