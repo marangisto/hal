a50c1c419886b92c3b2d6c2bc7b7639083c9fbe6
Bengt Marten Agren <maraingist@me.com>
2019-04-24 23:43:04 +0800

    Added bit-banging example using 74HC595.

---
+++ example/BitBang/Main.cpp
@@ -0,0 +1,55 @@
+////
+//
+//      Bit-bang 74HC595 in slow motion
+//
+////
+
+#include <gpio.h>
+
+using namespace stm32f0;
+using namespace gpio;
+
+typedef output_t<PA0> srclk;
+typedef output_t<PA1> ser;
+typedef output_t<PA2> rclk;
+typedef output_t<PC8> led_a;
+typedef output_t<PC9> led_b;
+
+static void send(uint8_t x)
+{
+    for (uint8_t i = 0; i < 8; ++i)
+    {
+        led_a::set();
+        sys_tick::delay_ms(25);
+        ser::write(x & BV(i));
+        srclk::set();
+        srclk::clear();
+        led_a::clear();
+        sys_tick::delay_ms(25);
+    }
+
+    led_b::set();
+    rclk::set();
+    sys_tick::delay_ms(25);
+    rclk::clear();
+    led_b::clear();
+}
+
+
+int main()
+{
+    srclk::setup();
+    ser::setup();
+    rclk::setup();
+    led_a::setup();
+    led_b::setup();
+
+    uint8_t i = 0;
+
+    for (;;)
+    {
+        send(i++);
+        sys_tick::delay_ms(250);
+    }
+}
+
