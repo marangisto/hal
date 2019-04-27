6ddacf6e65d4163a6d1def3558b88f8492df8eee
Bengt Marten Agren <maraingist@me.com>
2019-04-27 17:56:48 +0800

    Finished SPI HC595 example.

--- example/SPI/Main.cpp
+++ example/SPI/Main.cpp
@@ -1,3 +1,9 @@
+////
+// 
+//      Drive 74HC595 using SPI peripheral
+//
+////
+
 #include <spi.h>
 #include <gpio.h>
 
@@ -5,7 +11,7 @@ using namespace stm32f0;
 using namespace gpio;
 using namespace spi;
 
-typedef spi_t<1, PA5, PA7> display;
+typedef spi_t<1, PA5, PA7> hc595;
 typedef output_t<PA2> latch;
 typedef output_t<PC8> led_a;
 typedef output_t<PC9> led_b;
@@ -14,7 +20,7 @@ void loop();
 
 int main()
 {
-    display::setup<lsb_first, fpclk_2, low_speed>();
+    hc595::setup<lsb_first, fpclk_8, low_speed>();
     latch::setup();
     led_a::setup();
     led_b::setup();
@@ -25,7 +31,7 @@ int main()
 
 void loop()
 {
-    static uint16_t i = 0;
+    static uint8_t i = 0;
 
     if (i == 0)
     {
@@ -34,10 +40,10 @@ void loop()
             led_b::toggle();
     }
 
-    display::write(i++);
-    display::wait_idle();
+    hc595::write(i++);
+    hc595::wait_idle();
     latch::set();
     latch::clear();
-    sys_tick::delay_ms(1);
+    sys_tick::delay_ms(25);
 }
 
