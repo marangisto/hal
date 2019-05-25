8214943718722452f7a4ea936dcea54173be43ea
Marten Agren <marangisto@me.com>
2019-05-25 16:51:37 +0800

    Use traits to type 32-bit vs 16-bit timers.

--- include/timer.h
+++ include/timer.h
@@ -16,6 +16,7 @@ template<int TN> struct timer_traits {};
 template<> struct timer_traits<1>
 {
     typedef tim1_t T;
+    typedef uint16_t count_t;
     static inline T& TIM() { return TIM1; }
     static inline void rcc_enable() { RCC.APB2ENR |= BV(rcc_t::APB2ENR_TIM1EN); }
     static inline void nvic_enable() { NVIC.ISER |= BV(ISR::TIM1_BRK_UP_TRG_COM); }
@@ -26,6 +27,7 @@ template<> struct timer_traits<1>
 template<> struct timer_traits<2>
 {
     typedef tim2_t T;
+    typedef uint32_t count_t;
     static inline T& TIM() { return TIM2; }
     static inline void rcc_enable() { RCC.APB1ENR |= BV(rcc_t::APB1ENR_TIM2EN); }
     static inline void nvic_enable() { NVIC.ISER |= BV(ISR::TIM2); }
@@ -36,6 +38,7 @@ template<> struct timer_traits<2>
 template<> struct timer_traits<3>
 {
     typedef tim3_t T;
+    typedef uint16_t count_t;
     static inline T& TIM() { return TIM3; }
     static inline void rcc_enable() { RCC.APB1ENR |= BV(rcc_t::APB1ENR_TIM3EN); }
     static inline void nvic_enable() { NVIC.ISER |= BV(ISR::TIM3); }
@@ -47,6 +50,7 @@ template<> struct timer_traits<3>
 template<> struct timer_traits<6>
 {
     typedef tim6_t T;
+    typedef uint16_t count_t;
     static inline T& TIM() { return TIM6; }
     static inline void rcc_enable() { RCC.APB1ENR |= BV(rcc_t::APB1ENR_TIM6EN); }
     static inline void nvic_enable() { NVIC.ISER |= BV(ISR::TIM6_DAC); }
@@ -57,6 +61,7 @@ template<> struct timer_traits<6>
 template<> struct timer_traits<7>
 {
     typedef tim7_t T;
+    typedef uint16_t count_t;
     static inline T& TIM() { return TIM7; }
     static inline void rcc_enable() { RCC.APB1ENR |= BV(rcc_t::APB1ENR_TIM7EN); }
     static inline void nvic_enable() { NVIC.ISER |= BV(ISR::TIM7); }
@@ -66,6 +71,7 @@ template<> struct timer_traits<7>
 template<> struct timer_traits<14>
 {
     typedef tim14_t T;
+    typedef uint16_t count_t;
     static inline T& TIM() { return TIM14; }
     static inline void rcc_enable() { RCC.APB1ENR |= BV(rcc_t::APB1ENR_TIM14EN); }
     static inline void nvic_enable() { NVIC.ISER |= BV(ISR::TIM14); }
@@ -75,6 +81,7 @@ template<> struct timer_traits<14>
 template<> struct timer_traits<15>
 {
     typedef tim15_t T;
+    typedef uint16_t count_t;
     static inline T& TIM() { return TIM15; }
     static inline void rcc_enable() { RCC.APB2ENR |= BV(rcc_t::APB2ENR_TIM15EN); }
     static inline void nvic_enable() { NVIC.ISER |= BV(ISR::TIM15); }
@@ -84,6 +91,7 @@ template<> struct timer_traits<15>
 template<> struct timer_traits<16>
 {
     typedef tim16_t T;
+    typedef uint16_t count_t;
     static inline T& TIM() { return TIM16; }
     static inline void rcc_enable() { RCC.APB2ENR |= BV(rcc_t::APB2ENR_TIM16EN); }
     static inline void nvic_enable() { NVIC.ISER |= BV(ISR::TIM16); }
@@ -92,6 +100,7 @@ template<> struct timer_traits<16>
 template<> struct timer_traits<17>
 {
     typedef tim17_t T;
+    typedef uint16_t count_t;
     static inline T& TIM() { return TIM17; }
     static inline void rcc_enable() { RCC.APB2ENR |= BV(rcc_t::APB2ENR_TIM17EN); }
     static inline void nvic_enable() { NVIC.ISER |= BV(ISR::TIM17); }
@@ -101,7 +110,9 @@ template<int TN>
 class timer_t
 {
 public:
-    static inline void setup(uint16_t psc, uint16_t arr)
+    typedef typename timer_traits<TN>::count_t count_t;
+
+    static inline void setup(uint16_t psc, count_t arr)
     {
         timer_traits<TN>::rcc_enable();
         TIM().CR1 = _::CR1_RESET_VALUE;
@@ -127,7 +138,7 @@ public:
         TIM().SR &= ~BV(_::SR_UIF);
     }
 
-    static inline volatile uint16_t cnt()
+    static inline volatile count_t cnt()
     {
         return TIM().CNT;
     }
@@ -141,8 +152,10 @@ template<int TN, gpio::gpio_pin_t CH1, gpio::gpio_pin_t CH2>
 class encoder_t
 {
 public:
+    typedef typename timer_traits<TN>::count_t count_t;
+
     template<gpio::input_type_t input_type>
-    static inline void setup(uint16_t arr)
+    static inline void setup(count_t arr)
     {
         using namespace gpio;
 
@@ -152,13 +165,13 @@ public:
         timer_traits<TN>::rcc_enable();
         TIM().CCMR1 = _::CCMR1_RESET_VALUE | (0x1 << _::CCMR1_CC1S) | (0x1 << _::CCMR1_CC2S); // TI1 & TI2 as inputs
         TIM().CCER = _::CCER_RESET_VALUE;   // CCER_CC1P CCER_CC2P polarity choices
-        TIM().SMCR = _::SMCR_RESET_VALUE | (0x01 << _::SMCR_SMS);
+        TIM().SMCR = _::SMCR_RESET_VALUE | (0x1 << _::SMCR_SMS);
         TIM().ARR = arr;
         TIM().CNT = _::CNT_RESET_VALUE;;
         TIM().CR1 = _::CR1_RESET_VALUE | BV(_::CR1_CEN);
     }
 
-    static inline volatile uint16_t count()
+    static inline volatile count_t count()
     {
         return TIM().CNT;
     }
