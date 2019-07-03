0681f896562fdb5319b810b0115448bdbee340c2
Bengt Marten Agren <marangisto@me.com>
2019-07-03 21:34:11 +0800

    Added blinky example for STM32G070 Nucleo.

---
+++ examples/NUCLEO-G070RB/Blink/Main.cpp
@@ -0,0 +1,22 @@
+#include <gpio.h>
+
+using hal::sys_tick;
+using namespace hal::gpio;
+
+typedef output_t<PA5> ld4;
+
+void loop();
+
+int main()
+{
+    ld4::setup();
+    for (;;)
+        loop();
+}
+
+void loop()
+{
+    ld4::toggle();
+    sys_tick::delay_ms(25);
+}
+
