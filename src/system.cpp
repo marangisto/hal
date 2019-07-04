ad5af0b7fc5155845b5db2bc5b760221400cf15f
Bengt Marten Agren <marangisto@me.com>
2019-07-04 22:45:44 +0800

    Progressed clock initialization, needs verification.

--- src/system.cpp
+++ src/system.cpp
@@ -126,7 +126,7 @@ extern "C" void system_init(void)
     constexpr uint8_t pllM = 8;                         // 6 bits, valid range [2..63]
     constexpr pllP_t pllP = pllP_4;                     // 2 bits, enum range [2, 4, 6, 8]
     constexpr uint8_t pllQ = 9;                         // 4 bits, valid range [2..15]
-    constexpr uint8_t pllSRC = 0;                       // 0 = HSI, 1 = HSE 
+    constexpr uint8_t pllSRC = 0;                       // 1 bit, 0 = HSI, 1 = HSE
 
     using internal::encode;
 
@@ -139,7 +139,6 @@ extern "C" void system_init(void)
 
     RCC.CR |= _::CR_PLLON;                              // enable PLL
     while (!(RCC.CR & _::CR_PLLRDY));                   // wait for PLL to be ready
-
     RCC.CFGR |= encode<0x2, _::CFGR_SW0, 2>();          // select PLL as system clock
 
     // wait for PLL as system clock
@@ -155,22 +154,35 @@ extern "C" void system_init(void)
     RCC.CR = _::CR_RESET_VALUE;
     RCC.CFGR = _::CFGR_RESET_VALUE;
 
-    // set system clock to HSI-PLL 64MHz
+    // set system clock to HSI-PLL 64MHz and p-clock = 64MHz
 
-/*
-    constexpr uint8_t wait_states = 0x2;    // 2 wait-states for 64MHz
+    // fR (fSYS) = fVCO / pllP                          // <= 64MHz
+    // fP = fVCO / pllP                                 // <= 122MHz
 
-    FLASH.ACR = flash_t::ACR_PRFTEN | flash_t::ACR_LATENCY<wait_states>;
+    // pllN = 8.0, pllM = 1.0, pllP = 2.0, pllR = 2.0, fSYS = 6.4e7, fPllP = 6.4e7, fVCO = 1.28e8
 
-    RCC.CFGR |= _::CFGR_PLLMUL<0xa>;        // PLL multiplier 12
-    RCC.CR |= _::CR_PLLON;                  // enable PLL
-    while (!(RCC.CR & _::CR_PLLRDY));       // wait for PLL to be ready
-    RCC.CFGR |= _::CFGR_SW<0x2>;            // select PLL as system clock
+    constexpr uint16_t pllN = 8;                        // 7 bits, valid range [8..86]
+    constexpr uint8_t pllM = 1 - 1;                     // 3 bits, valid range [1..8]-1
+    constexpr uint8_t pllP = 2 - 1;                     // 5 bits, valid range [2..32]-1
+    constexpr uint8_t pllR = 2 - 1;                     // 3 bits, valid range [2..8]-1
+    constexpr uint8_t pllSRC = 2;                       // 2 bits, 2 = HSI16, 3 = HSE
+
+    RCC.PLLSYSCFGR = _::PLLSYSCFGR_PLLSRC<pllSRC>
+                   | _::PLLSYSCFGR_PLLN<pllN>
+                   | _::PLLSYSCFGR_PLLM<pllM>
+                   | _::PLLSYSCFGR_PLLP<pllP>
+                   | _::PLLSYSCFGR_PLLR<pllR>
+                   | _::PLLSYSCFGR_PLLPEN
+                   ;
+
+    RCC.CR |= _::CR_PLLON;                              // enable PLL
+    while (!(RCC.CR & _::CR_PLLRDY));                   // wait for PLL to be ready
+
+//    RCC.CFGR |= _::CFGR_SW<0x2>;                        // select PLL as system clock
 
     // wait for PLL as system clock
 
-    while ((RCC.CFGR & _::CFGR_SWS<0x3>) != _::CFGR_SWS<0x2>);
-*/
+//    while ((RCC.CFGR & _::CFGR_SWS<0x3>) != _::CFGR_SWS<0x2>);
 
     // initialize sys-tick for milli-second counts
 
