3679b5008bac60d4e43832c672bdcaa28ace7f02
Bengt Marten Agren <maraingist@me.com>
2019-07-07 12:45:28 +0800

    Simplified mco traits.

--- include/mco.h
+++ include/mco.h
@@ -11,33 +11,24 @@ namespace mco
 //using namespace gpio;
 
 enum mco_sel_t
-    { mco_off
-    , mco_lsi
-    , mco_lse
-    , mco_sysclk
-    , mco_hsi
-    , mco_hse
-    , mco_pll
+    { mco_off       = 0
+    , mco_lsi       = 6
+    , mco_lse       = 7
+    , mco_sysclk    = 1
+    , mco_hsi       = 3
+    , mco_hse       = 4
+    , mco_pll       = 5
     };
 
-template<int> struct mco_sel_traits {};
-template<> struct mco_sel_traits<mco_off> { static const uint8_t sel = 0; };
-template<> struct mco_sel_traits<mco_lsi> { static const uint8_t sel = 6; };
-template<> struct mco_sel_traits<mco_lse> { static const uint8_t sel = 7; };
-template<> struct mco_sel_traits<mco_sysclk> { static const uint8_t sel = 1; };
-template<> struct mco_sel_traits<mco_hsi> { static const uint8_t sel = 3; };
-template<> struct mco_sel_traits<mco_hse> { static const uint8_t sel = 4; };
-template<> struct mco_sel_traits<mco_pll> { static const uint8_t sel = 5; };
-
-template<int> struct mco_pre_traits {};
-template<> struct mco_pre_traits<1> { static const uint8_t pre = 0; };
-template<> struct mco_pre_traits<2> { static const uint8_t pre = 1; };
-template<> struct mco_pre_traits<4> { static const uint8_t pre = 2; };
-template<> struct mco_pre_traits<8> { static const uint8_t pre = 3; };
-template<> struct mco_pre_traits<16> { static const uint8_t pre = 4; };
-template<> struct mco_pre_traits<32> { static const uint8_t pre = 5; };
-template<> struct mco_pre_traits<64> { static const uint8_t pre = 6; };
-template<> struct mco_pre_traits<128> { static const uint8_t pre = 7; };
+template<int> struct mco_prescale {};
+template<> struct mco_prescale<1> { static const uint8_t value = 0; };
+template<> struct mco_prescale<2> { static const uint8_t value = 1; };
+template<> struct mco_prescale<4> { static const uint8_t value = 2; };
+template<> struct mco_prescale<8> { static const uint8_t value = 3; };
+template<> struct mco_prescale<16> { static const uint8_t value = 4; };
+template<> struct mco_prescale<32> { static const uint8_t value = 5; };
+template<> struct mco_prescale<64> { static const uint8_t value = 6; };
+template<> struct mco_prescale<128> { static const uint8_t value = 7; };
 
 template<gpio::gpio_pin_t PIN, mco_sel_t SEL, int PRESCALE>
 struct mco_t
@@ -49,8 +40,8 @@ struct mco_t
     {
         using device::RCC;
 
-        RCC.CFGR |= _::CFGR_MCOPRE<mco_pre_traits<PRESCALE>::pre>;
-        RCC.CFGR |= _::CFGR_MCOSEL<mco_sel_traits<SEL>::sel>;
+        RCC.CFGR |= _::CFGR_MCOPRE<mco_prescale<PRESCALE>::value>;
+        RCC.CFGR |= _::CFGR_MCOSEL<SEL>;
         mco::template setup<gpio::high_speed>();
     }
 };
