b2c5823158b0a8f8009506521fbd09b31d030b7e
Bengt Marten Agren <maraingist@me.com>
2019-07-07 13:27:46 +0800

    Adapted MCO to work on STM32G431.

---
+++ examples/NUCLEO-G431RB/MCO/Main.cpp
@@ -0,0 +1,27 @@
+#include <gpio.h>
+#include <mco.h>
+
+using hal::sys_tick;
+using namespace hal::gpio;
+using namespace hal::mco;
+
+typedef output_t<PA5> ld4;
+typedef mco_t<PA8, mco_hsi16, 4> mco;
+
+void loop();
+
+int main()
+{
+    ld4::setup();
+    mco::setup();
+
+    for (;;)
+        loop();
+}
+
+void loop()
+{
+    ld4::toggle();
+    sys_tick::delay_ms(1000);
+}
+
--- include/mco.h
+++ include/mco.h
@@ -12,15 +12,18 @@ namespace mco
 
 enum mco_sel_t
     { mco_off       = 0
-    , mco_lsi       = 6
-    , mco_lse       = 7
     , mco_sysclk    = 1
-    , mco_hsi       = 3
+    , mco_hsi16     = 3
     , mco_hse       = 4
     , mco_pll       = 5
+    , mco_lsi       = 6
+    , mco_lse       = 7
+    , mco_hsi48     = 8     // only on STM32G4
     };
 
 template<int> struct mco_prescale {};
+
+#if defined(STM32G070)
 template<> struct mco_prescale<1> { static const uint8_t value = 0; };
 template<> struct mco_prescale<2> { static const uint8_t value = 1; };
 template<> struct mco_prescale<4> { static const uint8_t value = 2; };
@@ -29,6 +32,13 @@ template<> struct mco_prescale<16> { static const uint8_t value = 4; };
 template<> struct mco_prescale<32> { static const uint8_t value = 5; };
 template<> struct mco_prescale<64> { static const uint8_t value = 6; };
 template<> struct mco_prescale<128> { static const uint8_t value = 7; };
+#elif defined(STM32G431)
+template<> struct mco_prescale<1> { static const uint8_t value = 0; };
+template<> struct mco_prescale<2> { static const uint8_t value = 1; };
+template<> struct mco_prescale<4> { static const uint8_t value = 2; };
+template<> struct mco_prescale<8> { static const uint8_t value = 3; };
+template<> struct mco_prescale<16> { static const uint8_t value = 4; };
+#endif
 
 template<gpio::gpio_pin_t PIN, mco_sel_t SEL, int PRESCALE>
 struct mco_t
