fe16a58479bf16acb7a1adf40d23c941fb3e7e03
Bengt Marten Agren <maraingist@me.com>
2019-08-09 15:10:46 +0800

    Use timer to sample at 96kHz.

--- examples/NUCLEO-G431RB/DSP/Main.cpp
+++ examples/NUCLEO-G431RB/DSP/Main.cpp
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <usart.h>
 #include <redirect.h>
+#include <timer.h>
 #include <adc.h>
 #include <dac.h>
 
@@ -8,10 +9,12 @@ using hal::sys_tick;
 using hal::sys_clock;
 using namespace hal::gpio;
 using namespace hal::usart;
+using namespace hal::timer;
 using namespace hal::adc;
 using namespace hal::dac;
 
 typedef usart_t<2, PA2, PA3> serial;
+typedef hal::timer::timer_t<6> sample_timer;
 typedef output_t<PA10> d2;
 typedef analog_t<PA0> ain;
 typedef adc_t<1> adc;
@@ -24,6 +27,13 @@ template<> void handler<interrupt::USART2>()
     serial::isr();
 }
 
+template<> void handler<interrupt::TIM6_DACUNDER>()
+{
+    sample_timer::clear_uif();
+    d2::set();
+    d2::clear();
+}
+
 int main()
 {
     dac::setup();
@@ -33,10 +43,14 @@ int main()
     adc::setup();
     ain::setup();
 
+    sample_timer::setup(8, 196);
+    sample_timer::update_interrupt_enable();
+
     d2::setup();
  
     serial::setup<230400>();
     hal::nvic<interrupt::USART2>::enable();
+    hal::nvic<interrupt::TIM6_DACUNDER>::enable();
     interrupt::enable();
     stdio_t::bind<serial>();
 
@@ -48,10 +62,8 @@ int main()
 
 void loop()
 {
-    d2::toggle();
     adc::start_conversion();
     while (!adc::end_of_conversion());
-    d2::toggle();
     uint16_t y = adc::read();
     dac::write<1>(y);
     dac::write<2>(4095-y);
