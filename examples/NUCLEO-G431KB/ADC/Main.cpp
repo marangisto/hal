ab54e1d94b215f0d534c31dc0dbb39192e7f6902
Bengt Marten Agren <maraingist@me.com>
2019-08-19 21:38:53 +0800

    Initial ADC example for NUCLEO-G431KB board.

---
+++ examples/NUCLEO-G431KB/ADC/Main.cpp
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <cstring>
+#include <usart.h>
+#include <redirect.h>
+#include <adc.h>
+
+using hal::sys_tick;
+using namespace hal::gpio;
+using namespace hal::usart;
+using namespace hal::adc;
+
+typedef usart_t<2, PA2, PA3> serial;
+typedef output_t<PB8> led;
+typedef hal::adc::adc_t<1> adc;
+
+template<> void handler<interrupt::USART2>()
+{
+    led::toggle();
+    serial::isr();
+}
+
+int main()
+{
+    led::setup();
+    serial::setup<230400>();
+    hal::nvic<interrupt::USART2>::enable();
+    interrupt::enable();
+
+    stdio_t::bind<serial>();
+    printf("Welcome to the STM32G431!\n");
+
+    adc::setup();
+    adc::sequence<1, 2>();
+    adc::enable();
+
+    for (;;)
+    {
+        uint16_t x = adc::read();
+
+        printf("%d\n", x);
+        sys_tick::delay_ms(50);
+    }
+}
