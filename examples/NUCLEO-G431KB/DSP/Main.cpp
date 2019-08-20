18e1c4bf154edae55fddf9018eacc1a84f99887a
Bengt Marten Agren <maraingist@me.com>
2019-08-20 21:17:42 +0800

    Clean up DSP pipeline example.

--- examples/NUCLEO-G431KB/DSP/Main.cpp
+++ examples/NUCLEO-G431KB/DSP/Main.cpp
@@ -17,17 +17,18 @@ typedef timer_t<3> aux;
 
 typedef hal::adc::adc_t<1> adc;
 typedef hal::dac::dac_t<1> dac;
-typedef hal::dma::dma_t<1> dma;
+typedef hal::dma::dma_t<1> adc_dma;
+typedef hal::dma::dma_t<2> dac_dma;
 
 static const uint8_t dac_dma_ch = 1;
-static const uint8_t adc_dma_ch = 2;
-static const uint16_t half_buffer_size = 128;
+static const uint8_t adc_dma_ch = 1;
+static const uint16_t half_buffer_size = 256;
 static const uint16_t buffer_size = half_buffer_size * 2;
 static uint16_t input_buffer[buffer_size];
 static uint16_t output_buffer[buffer_size];
-static const uint32_t sample_freq = 96000;
+static const uint32_t sample_freq = 192000;
 
-typedef button_t<PC13> btn;
+typedef button_t<PB3> btn;
 typedef output_t<PB8> led;
 typedef output_t<PB4> probe;
 typedef analog_t<PA0> ain;
@@ -47,20 +48,10 @@ template<> void handler<interrupt::TIM6_DACUNDER>()
 
 template<> void handler<interrupt::DMA1_CH1>()
 {
-    uint32_t sts = dma::interrupt_status<dac_dma_ch>();
+    uint32_t sts = adc_dma::interrupt_status<adc_dma_ch>();
 
-    dma::clear_interrupt_flags<dac_dma_ch>();
-
-    if (sts & (dma_half_transfer | dma_transfer_complete))
-        probe::write(sts & dma_transfer_complete);
-}
-
-template<> void handler<interrupt::DMA1_CH2>()
-{
-    uint32_t sts = dma::interrupt_status<adc_dma_ch>();
-
-    dma::clear_interrupt_flags<adc_dma_ch>();
-    led::set();
+    adc_dma::clear_interrupt_flags<adc_dma_ch>();
+    probe::set();
 
     if (sts & (dma_half_transfer | dma_transfer_complete))
     {
@@ -71,7 +62,7 @@ template<> void handler<interrupt::DMA1_CH2>()
             *q++ = *p++;
     }
 
-    led::clear();
+    probe::clear();
 }
 
 int main()
@@ -86,28 +77,27 @@ int main()
 
     interrupt::enable();
 
-    dma::setup();
-    hal::nvic<interrupt::DMA1_CH1>::enable();
-    hal::nvic<interrupt::DMA1_CH2>::enable();
-
-    dac::setup();
-    dac::enable_trigger<1, 7>();    // FIXME: use constant for TIM6_TRGO
-    dac::enable_dma<1, dma, dac_dma_ch, uint16_t>(output_buffer, buffer_size);
-    dma::enable_interrupt<dac_dma_ch, true>();
-
-    ain::setup();
-    adc::setup();
-    adc::sequence<1>();
-    adc::dma<dma, adc_dma_ch, uint16_t>(input_buffer, buffer_size);
-    adc::trigger<0xd>();            // FIXME: use constant for TIM6_TRGO
-    adc::enable();
-    adc::start_conversion();
-
     tim6::setup(0, sys_clock::freq() / sample_freq - 1);
     tim6::master_mode<tim6::mm_update>();
     // enable for sampling frequency probe
     //tim6::update_interrupt_enable();
     //hal::nvic<interrupt::TIM6_DACUNDER>::enable();
+
+    dac_dma::setup();
+    adc_dma::setup();
+    hal::nvic<interrupt::DMA1_CH1>::enable();
+
+    dac::setup();
+    dac::enable_trigger<1, 7>();    // FIXME: use constant for TIM6_TRGO
+    dac::enable_dma<1, dac_dma, dac_dma_ch>(output_buffer, buffer_size);
+
+    ain::setup();
+    adc::setup();
+    adc::sequence<1>();
+    adc::dma<adc_dma, adc_dma_ch>(input_buffer, buffer_size);
+    adc::trigger<0xd>();            // FIXME: use constant for TIM6_TRGO
+    adc::enable();
+    adc::start_conversion();
 
     for (;;)
     {
