3e100ec75d9bda1d71dceb2f63b0cdf8967bb5ba
Bengt Marten Agren <marangisto@me.com>
2019-11-05 21:56:31 +0800

    Started working on enum generation for alternate functions.

--- util/AltFun.hs
+++ util/AltFun.hs
@@ -1,18 +1,47 @@
+{-# LANGUAGE DeriveDataTypeable, RecordWildCards, OverloadedStrings #-}
 module AltFun (main) where
 
+import System.Console.CmdArgs
 import System.Environment
+import Data.List (nub, sort)
 import Data.Maybe
 import System.IO
 
+data Options = Options
+    { enum      :: Bool
+    , files     :: [FilePath]
+    } deriving (Show, Eq, Data, Typeable)
+
+options :: Options
+options = Options
+    { enum = def &= help "generate enumeration (one or more files as input)"
+    , files = def &= args &= typ "FILES"
+    } &=
+    verbosity &=
+    help "Generate alternate function traits from txt files" &=
+    summary "AltFun v0.0.0, (c) Bengt Marten Agren 2019" &=
+    details [ "SVD2CPP generated device header files for ARM-based"
+            , "MCUs based on vendor SVD files (see CMSIS)."
+            ]
+
 main :: IO ()
 main = do
+    opts@Options{..} <- cmdArgs options
     hSetNewlineMode stdout noNewlineTranslation
-    [file] <- getArgs
-    xs <- lines <$> readFile file
-    let ys = concatMap (process . words) xs
-    mapM_ putStrLn ys
+    case (enum, files) of
+        (False, (file:[])) -> do
+            xs <- lines <$> readFile file
+            let ys = concatMap (traits . parse . words) xs
+            mapM_ putStrLn ys
+        (True, files) -> do
+            xs <- concatMap lines <$> mapM readFile files
+            let ys = nub . sort $ concatMap (enums . parse . words) xs
+            mapM_ putStrLn ys
+        _ -> error "use:\n    AltFun file.txt\n    AltFun --enum file.txt [file.txt...]"
 
-process (x:xs) = mapMaybe (uncurry $ decl x) $ concatMap multi $ zip xs [0..]
+parse (x:xs) = (x, concatMap multi $ zip xs [0..])
+traits (x, xs) = mapMaybe (uncurry $ decl x) xs
+enums (x, xs) = filter (/="-") $ map fst xs
 
 decl p "-" _ = Nothing
 decl p f i = Just $ mconcat
