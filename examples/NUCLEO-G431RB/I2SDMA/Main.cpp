714c00d4f4d45d4ab4f8b08281fab3fb36854a70
Bengt Marten Agren <maraingist@me.com>
2019-11-19 21:38:46 +0800

    Two separate channels in single dma buffer for i2s.

--- examples/NUCLEO-G431RB/I2SDMA/Main.cpp
+++ examples/NUCLEO-G431RB/I2SDMA/Main.cpp
@@ -16,8 +16,8 @@ typedef i2s_t<2, PB13, PA11, PB12> i2s;
 typedef hal::dma::dma_t<1> i2sdma;
 static const uint8_t i2sdma_ch = 1;
 
-static const uint16_t buf_size = 128;
-static uint16_t bufa[buf_size];
+static const uint16_t buf_size = 128;   // samples per channel
+static uint16_t bufa[buf_size * 2];     // interleave both channels
 
 void loop();
 
@@ -26,11 +26,15 @@ int main()
     ld4::setup();
 
     for (uint16_t i = 0; i < buf_size; ++i)
-        bufa[i] = i * (0xffff / buf_size);
+    {
+        uint16_t j = i << 1;
+        bufa[j] = i * (0xffff / buf_size);
+        bufa[j + 1] = 0xffff - bufa[j];
+    }
 
     i2sdma::setup();
-    i2s::setup<philips_i2s, low_level, format_32_32, 27>();
-    i2s::enable_dma<i2sdma, i2sdma_ch>(bufa, buf_size);
+    i2s::setup<philips_i2s, low_level, format_16_16, 27>();
+    i2s::enable_dma<i2sdma, i2sdma_ch>(bufa, buf_size * 2);
 
     for (;;)
         loop();
