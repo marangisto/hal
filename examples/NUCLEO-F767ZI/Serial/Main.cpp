e918b63871080417087c66f5791aa2e749c29270
Bengt Marten Agren <marangisto@me.com>
2020-01-06 20:53:53 +0800

    Added serial example for STM32F767 (partially tested).

---
+++ examples/NUCLEO-F767ZI/Serial/Main.cpp
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <usart.h>
+#include <redirect.h>
+#include <cstring>
+
+using hal::sys_tick;
+using namespace hal::gpio;
+using namespace hal::usart;
+
+typedef usart_t<3, PD8, PD9> serial;
+typedef output_t<PB0> led;
+
+void loop();
+
+template<> void handler<interrupt::USART3>()
+{
+    led::toggle();
+    serial::isr();
+}
+
+int main()
+{
+    led::setup();
+    serial::setup<230400>();
+    hal::nvic<interrupt::USART3>::enable();
+    interrupt::enable();
+
+    stdio_t::bind<serial>();
+    printf("Welcome to the STM32F767!\n");
+
+    for (;;)
+        loop();
+}
+
+void loop()
+{
+    char buf[256];
+
+    printf("> \n");
+    if (fgets(buf, sizeof(buf), stdin))
+    {
+        buf[strcspn(buf, "\r\n")] = 0;
+        printf("got = '%s'\n", buf);
+    }
+}
+
