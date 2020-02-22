faca2a2358640e67d59f67b4cef16d1108768798
Bengt Marten Agren <maraingist@me.com>
2020-02-22 13:13:33 +0800

    Approximate but more general fuzz for micro-second timing.

---
+++ examples/NUCLEO-G431KB/MuSec/Main.cpp
@@ -0,0 +1,22 @@
+#include <gpio.h>
+
+using hal::sys_tick;
+using namespace hal::gpio;
+
+typedef output_t<PA9> probe;
+
+void loop();
+
+int main()
+{
+    probe::setup();
+
+    for (;;)
+    {
+        probe::set();
+        sys_tick::delay_us(1);
+        probe::clear();
+        sys_tick::delay_us(20);
+    }
+}
+
