12c1158e268c28bedbda20cd47c4ab3cf2defa44
Bengt Marten Agren <maraingist@me.com>
2020-02-29 18:20:24 +0800

    Added miniml list container support (moved from Beats project).

--- include/list.h
+++ include/list.h
@@ -23,12 +23,6 @@ struct list_iterator
         return *this;
     }
 
-    list_iterator& operator--()
-    {
-        p = p->prev;
-        return *this;
-    }
-
     T& operator*() const
     {
         return p->value;
